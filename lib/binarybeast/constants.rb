d6a5bdeb5866d052eeceeed7785a33e740b1c088
Brandon Simmons <brandon@binarybeast.com>
2012-12-24 21:25:02 -0500

    Adds constants into BinaryBeast module

---
+++ lib/binarybeast/constants.rb
@@ -0,0 +1,32 @@
+module BinaryBeast
+  
+  # Constants
+  # ---------------------
+  BRACKET_GROUPS                  = 0
+  BRACKET_WINNERS                 = 1
+  BRACKET_LOSERS                  = 2
+  BRACKET_FINALS                  = 3
+  BRACKET_BRONZE                  = 4
+  # ---------------------
+  ELIMINATION_SINGLE              = 1
+  ELIMINATION_DOUBLE              = 2
+  # ---------------------
+  # Brackets
+  TOURNAMENT_TYPE_BRACKETS        = 0
+  # Round Robin -> Brackets
+  TOURNAMENT_TYPE_CUP             = 1
+  # ---------------------
+  SEEDING_RANDOM                  = 'random'
+  SEEDING_SPORTS                  = 'sports'
+  SEEDING_BALANCED                = 'balanced'
+  SEEDING_MANUAL                  = 'manual'
+  # ---------------------
+  REPLAY_DOWNLOADS_DISABLED       = 0
+  REPLAY_DOWNLOADS_ENABLED        = 1
+  REPLAY_DOWNLOADS_POST_COMPLETE  = 2
+  # ---------------------
+  REPLAY_UPLOADS_DISABLED         = 0
+  REPLAY_UPLOADS_OPTIONAL         = 1
+  REPLAY_UPLOADS_MANDATORY        = 2
+  
+end

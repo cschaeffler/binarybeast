c096cede781a03edfca2a49f72d039b095e11673
Brandon Simmons <brandon@binarybeast.com>
2012-12-24 21:26:04 -0500

    Renames module to BinaryBeast to be consistent with other API libraries
    Also, the call method in the module now allows providing a data block for yielding the result

--- lib/binarybeast.rb
+++ lib/binarybeast.rb
@@ -10,25 +10,27 @@ require "HTTParty"
 # ---------------------
 # does contain all the classes that interact with binarybeast api.
 
-module Binarybeast
+module BinaryBeast
   include HTTParty
   # Base URI for the BinaryBeast API
-  base_uri 'https://binarybeast.com/api'
+  base_uri 'https://api.binarybeast.com/'
   # Set HTTParty standard format to JSON, its standard what binarybeast api returns.
   format :json
   # Default api_key, this one is taken from my personal account.
-  @@api_key = "3c8955130c6e1406420d6202649651fe.50c9faa593f9c6.00530099"
-  
-  # Autoload Tourney Class when needed
-  autoload :Tourney, 'binarybeast/tourney'
-  # Autoload Team Class when needed
-  autoload :Team, 'binarybeast/team'
+  @@api_key = '3c8955130c6e1406420d6202649651fe.50c9faa593f9c6.00530099'
+
+  # Autoload the service classes
+  #autoload :Service,        'binarybeast/service'
+  autoload :Tournament,     'binarybeast/tournament'
+  autoload :Team,           'binarybeast/team'
+  autoload :Game,           'binarybeast/game'
+  require                   'binarybeast/constants'
 
   # Getter for api_key Variable
   def self.api_key
     @@api_key
   end
-  
+
   # Setter for api_key Variable
   def self.api_key=(api_key)
     @@api_key=api_key
@@ -37,14 +39,26 @@ module Binarybeast
   # Call
   # Method
   # ------------
-  # Raw Call Method for those who want to make raw calls on the API of Binarybeast. APIKey will be taken from the module variable
+  # Raw Call Method for those who want to make raw calls on the API of Binarybeast. api_key will be taken from the module variable
   # Example:
-  # tourney = Binarybeast.call(:APIService => "Tourney.TourneyCreate.Create", :title => "Test")
+  # tourney = Binarybeast.call('Tourney.TourneyCreate.Create', :title => "Test")
   # ------------
 
-  def self.call(options={})
-    options.merge!({:APIKey => @@api_key})
-    return self.get("", :query => options)
+  def self.call(service, options={})
+    options.merge!({
+      :api_key              => @@api_key,
+      :api_use_underscores  => true,
+      :api_service          => service
+    })
+
+    response = self.get('', :query => options)
+
+    unless block_given?
+      return response
+    else
+      yield response
+    end
+
   end
 
 end
